10e4a18052eb639ed1f13e35886b3b5d06f24c75
JHOUJHOU <a01450887@gmail.com>
2022-03-16 19:31:11 +0800

    SCSS樣式沒有被覆蓋

---
+++ src/views/FrontView.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+      <router-link to="/" class="text-primary">首頁</router-link> |
+      <router-link to="/products">商品列表</router-link> |
+      <router-link to="/login">前台購物車</router-link> |
+      <router-link to="/login">後台購物車列表</router-link> |
+
+      <!-- 往後台 -->
+      <router-link to="/admin/products">後台產品列表</router-link>
+
+    </nav>
+    <p class="text-primary">首頁</p>
+    <router-view/>
+</template>
