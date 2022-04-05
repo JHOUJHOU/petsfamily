338e06aa1b7b87283cb5bb9a9e46ef85212ff99e
JHOUJHOU <a01450887@gmail.com>
2022-04-05 21:01:24 +0800

    修正漢堡選單無法正常折疊

--- src/components/Front/FrontNav.vue
+++ src/components/Front/FrontNav.vue
@@ -1,28 +1,15 @@
 <template>
   <header class="bg-primary">
     <div class="container-lg">
-      <nav class="navbar navbar-expand-lg navbar-light">
+      <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <div class="container-fluid">
-          <router-link class="navbar-brand" to="/">
+          <div class="d-flex">
+            <router-link class="navbar-brand" to="/">
             <dog-side-icon/>寵愛
-          </router-link>
-          <!-- 漢堡選單 -->
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbar"
-            aria-controls="navbar"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse d-flex justify-content-between" id="navbar">
-            <ul class="navbar-nav">
+            </router-link>
+            <ul class="navbar-nav d-lg-flex d-lg-block d-none">
               <li class="nav-item">
-                <router-link to="/"
-                class="nav-link active"
-                >首頁</router-link>
+                <router-link to="/" class="nav-link active">首頁</router-link>
               </li>
               <li class="nav-item">
                 <router-link to="/products" class="nav-link">商品列表</router-link>
@@ -31,30 +18,60 @@
                 <router-link to="/question" class="nav-link">常見問題</router-link>
               </li>
             </ul>
-            <ul class="navbar-nav d-none d-md-block">
-              <li class="nav-item">
-                <router-link to="/cart" class="nav-link position-relative"><BIconCartCheckFill/>
-                <span
-                  class="position-absolute
-                  top-30
-                  start-90
-                  translate-middle
-                  badge
-                  rounded-pill
-                  bg-danger"
-                >
-                  {{ cartData?.carts?.length }}
-                </span>
+          </div>
+          <div>
+            <ul class="navbar-nav">
+              <li class="nav-item d-lg-block d-none">
+                <router-link to="/cart"
+                  class="nav-link position-relative">
+                  <BIconCartCheckFill/>
+                    <span
+                      class="position-absolute
+                        top-30
+                        start-90
+                        translate-middle
+                        badge
+                        rounded-pill
+                        bg-danger"
+                    >
+                      {{ cartData?.carts?.length }}
+                    </span>
                 </router-link>
               </li>
-              <!-- <li class="nav-item">
-                <router-link to="/contact" class="nav-link"><BIconTelephoneForwardFill/>
-                </router-link>
-              </li> -->
+              <li>
+                <button class="navbar-toggler" type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
+      <div class="navbar navbar-expand-lg navbar-light bg-primary d-lg-none d-block">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <router-link to="/" class="nav-link active">首頁</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/products" class="nav-link">商品列表</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/question" class="nav-link">常見問題</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/cart" class="nav-link">購物車</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
